1fdc291f6fdff713ec942d315738c189820a9845
Giovanni Toffetti <toff@zhaw.ch>
2019-12-03 20:24:54 +0000

    script to start with gpu

---
+++ workspace_included/run-with-gpu.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+export TAG=latest
+export BASE_IMAGE=robopaas/rosdocked-kinetic-workspace-included-gpu-manual
+export IMAGE_NAME=$BASE_IMAGE:$TAG
+
+set -e
+
+nvidia-docker run --privileged --net=host -e PATH=/usr/local/cuda/bin/:$PATH -e LD_LIBRARY_PATH=/usr/local/cuda/lib64 -e NVIDIA_VISIBLE_DEVICES=all -e NVIDIA_DRIVER_CAPABILITIES=compute,utility -v /usr/local/cuda-10.2:/usr/local/cuda -it $IMAGE_NAME $SHELL
