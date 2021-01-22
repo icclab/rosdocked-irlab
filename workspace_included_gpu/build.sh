910b8cc11d6be4222718abfb96b3d7493c282c49
Giovanni Toffetti <toff@zhaw.ch>
2021-01-22 13:13:35 +0000

    Built gpu container for kinetic. Grasping demo fails at initial arm movement

---
+++ workspace_included_gpu/build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+export IMAGE_NAME=robopaas/rosdocked-kinetic-workspace-included-gpu:latest
+
+# Get this script's path
+pushd `dirname $0` > /dev/null
+SCRIPTPATH=`pwd`
+popd > /dev/null
+
+# Build the docker image
+docker build \
+  --build-arg USER=$USER\
+  --build-arg uid=$UID\
+  --build-arg HOME=/home/ros \
+  --build-arg WORKSPACE=/home/ros \
+  --build-arg shell=$SHELL\
+  -t $IMAGE_NAME .
---
+++ workspace_included_gpu/run_gpu.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+nvidia-docker run --init --name=ros-gpu --rm -it -e DISPLAY=:1 -v /tmp/.X11-unix/X0:/tmp/.X11-unix/X0:rw -p 443:40001 robopaas/rosdocked-kinetic-workspace-included-gpu:latest
