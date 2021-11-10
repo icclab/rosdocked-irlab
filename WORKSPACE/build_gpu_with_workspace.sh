448684712d1daa45c53618d7a670d2901836eb6b
MarkStraub <web@markstraub.dev>
2021-11-10 15:30:46 +0000

    Add Workspace Docker to build the cpu and gpu workspace.

---
+++ WORKSPACE/build_gpu_with_workspace.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+################################################################################
+# ZHAW INIT
+# Description:  Shell script to create and add the required elements to the
+#               CPU image
+# Authors:      Leonardo Militano, Mark Straub, Giovanni Toffetti
+# Date:         2021-11-08
+################################################################################
+
+export IMAGE_NAME=robopaas/rosdocked-noetic-gpu:latest
+
+# Build the docker image
+docker build \
+  --build-arg BASE_IMAGE=robopaas/rosdocked-noetic-base-gpu:latest \
+  --build-arg home=/home/ros \
+  --build-arg workspace=/home/ros \
+  --build-arg shell=$SHELL\
+  -t $IMAGE_NAME .
