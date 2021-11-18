92c7c2b092f6a4a0e588788cfd5d80cf8de1db33
Giovanni Toffetti <toff@zhaw.ch>
2021-11-18 13:16:14 +0000

    Added k8s base image

---
+++ BASE_K8S/build.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+################################################################################
+# ZHAW INIT
+# Description:  Shell script to create the Base GPU Docker image
+# Authors:      Leonardo Militano, Mark Straub, Giovanni Toffetti
+# Date:         2021-11-08
+################################################################################
+
+export IMAGE_NAME=robopaas/rosdocked-noetic-base-k8s:latest
+
+# Get this script's path
+pushd `dirname $0` > /dev/null
+SCRIPTPATH=`pwd`
+popd > /dev/null
+
+# Build the docker image
+docker build \
+  --build-arg user=user\
+  --build-arg uid=$UID\
+  --build-arg home=/home/user \
+  --build-arg workspace=/home/user \
+  --build-arg shell=$SHELL\
+  -t $IMAGE_NAME .
---
+++ WORKSPACE/build_k8s_with_workspace.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+################################################################################
+# ZHAW INIT
+# Description:  Shell script to create and add the required elements to the
+#               CPU image
+# Authors:      Leonardo Militano, Mark Straub, Giovanni Toffetti
+# Date:         2021-11-08
+################################################################################
+
+export IMAGE_NAME=robopaas/rosdocked-noetic-k8s:latest
+
+# Build the docker image
+docker build \
+  --build-arg BASE_IMAGE=robopaas/rosdocked-noetic-base-k8s:latest \
+  --build-arg USER=user \
+  --build-arg shell=$SHELL\
+  -t $IMAGE_NAME .
