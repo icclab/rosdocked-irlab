6accf025b9491577bb9e0280ea491e4e257e5c18
Leonardo Militano <39118873+miltzhaw@users.noreply.github.com>
2022-01-12 16:11:29 +0100

    Update run_nav_test_bash_k8s.sh

--- test/run_nav_test_bash_k8s.sh
+++ test/run_nav_test_bash_k8s.sh
@@ -5,7 +5,7 @@ export IMAGE=robopaas/rosdocked-noetic-k8s:latest
 export CONTAINER_NAME=nav_test_k8s
 echo $CONTAINER_NAME
 
-docker run -d -i -name $CONTAINER_NAME $IMAGE  /bin/bash
+docker run -d -i --name $CONTAINER_NAME $IMAGE  /bin/bash
 
 # TEST #1: testing navigation stack
 # test with bash
