4e0648276095b57440e10b4b67dd8468feec76f5
Leonardo Militano <39118873+miltzhaw@users.noreply.github.com>
2022-01-12 10:34:47 +0100

    Create run_nav_test_bash_k8s.sh

---
+++ test/run_nav_test_bash_k8s.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+export IMAGE=robopaas/rosdocked-noetic-k8s:latest
+
+docker run -d -i $IMAGE /bin/bash
+
+export CONTAINER_NAME=$(docker ps --latest --format "{{.Names}}")
+echo $CONTAINER_NAME
+
+# TEST #1: testing navigation stack
+# test with bash
+echo "Testing navigation stack..."
+docker exec -i $CONTAINER_NAME ./catkin_ws/src/icclab_summit_xl/.ci/nav_test_bash.sh
+if [[ "$?" == "0" ]] ; then
+  echo "Navigation test failed. Check output."
+  echo "Killing docker container $CONTAINER_NAME..."
+  docker kill $CONTAINER_NAME
+  docker rm $CONTAINER_NAME
+  exit 1
+elif [[ "$?" == "1" ]] ; then
+  echo "Navigation test succeeded! No issues found."
+  echo "Killing docker container $CONTAINER_NAME..."
+  docker kill $CONTAINER_NAME
+  docker rm $CONTAINER_NAME
+  exit 0
+else
+  echo "State of test unknown. Check output."
+  echo "Killing docker container $CONTAINER_NAME..."
+  docker kill $CONTAINER_NAME
+  docker rm $CONTAINER_NAME
+  exit 1
+fi
