700930ed6e4cc4fdcee9f5338291cde33c2e9388
Leonardo Militano <39118873+miltzhaw@users.noreply.github.com>
2022-01-12 09:12:08 +0100

    temp remove no-cache option

--- WORKSPACE/build_k8s_with_workspace.sh
+++ WORKSPACE/build_k8s_with_workspace.sh
@@ -12,7 +12,7 @@ export IMAGE_NAME=robopaas/rosdocked-noetic-k8s:latest
 
 # Build the docker image
 docker build \
-  --no-cache \
+#  --no-cache \
   --build-arg BASE_IMAGE=robopaas/rosdocked-noetic-base-k8s:latest \
   --build-arg USER=ros \
   --build-arg shell=$SHELL\
