24efd773a5aaef94a0d8d8441b7fbf2fc854517b
Leonardo Militano <39118873+miltzhaw@users.noreply.github.com>
2022-01-14 11:40:29 +0100

    Update run_grasp_test_bash_gpu.sh

--- test/run_grasp_test_bash_gpu.sh
+++ test/run_grasp_test_bash_gpu.sh
@@ -14,20 +14,17 @@ echo "Testing grasping stack..."
 docker exec -i $CONTAINER_NAME ./catkin_ws/src/icclab_summit_xl/.ci/grasp_test_bash.sh
 if [[ "$?" == "0" ]] ; then
   echo "Grasping test failed. Check output."
-  echo "Killing docker container $CONTAINER_NAME..."
-  docker kill $CONTAINER_NAME
-  docker rm $CONTAINER_NAME
+  echo "Removing docker container $CONTAINER_NAME..."
+  docker rm -f $CONTAINER_NAME || true
   exit 1
 elif [[ "$?" == "1" ]] ; then
   echo "Grasping test succeeded! No issues found."
-  echo "Killing docker container $CONTAINER_NAME..."
-  docker kill $CONTAINER_NAME
-  docker rm $CONTAINER_NAME
+  echo "Removing docker container $CONTAINER_NAME..."
+  docker rm -f $CONTAINER_NAME || true
   exit 0
 else
   echo "State of test unknown. Check output."
-  echo "Killing docker container $CONTAINER_NAME..."
-  docker kill $CONTAINER_NAME
-  docker rm $CONTAINER_NAME
+  echo "Removing docker container $CONTAINER_NAME..."
+  docker rm -f $CONTAINER_NAME || true
   exit 1
 fi
