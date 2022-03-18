3e7e326b84849e6f0d04ad48190e3262836d6096
Giovanni Toffetti <toff@zhaw.ch>
2022-03-18 10:23:03 +0000

    corrected foxy into noetic

--- BASE_K8S/build.sh
+++ BASE_K8S/build.sh
@@ -6,8 +6,8 @@
 # Authors:      Leonardo Militano, Mark Straub, Giovanni Toffetti
 # Date:         2021-11-08
 ################################################################################
-
-export IMAGE_NAME=robopaas/rosdocked-noetic-base-k8s:latest
+export ROS_DISTRO=noetic
+export IMAGE_NAME=robopaas/rosdocked-${ROS_DISTRO}-base-k8s:latest
 
 # Get this script's path
 pushd `dirname $0` > /dev/null
@@ -16,6 +16,7 @@ popd > /dev/null
 
 # Build the docker image
 docker build \
+  --no-cache \
   --build-arg user=user\
   --build-arg uid=$UID\
   --build-arg home=/home/user \
