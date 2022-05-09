7716c5ea26405dfe5c4b1bd8af2966cb0366f7a8
MarkStraub <web@markstraub.dev>
2022-05-09 10:49:57 +0200

    Add Base for k82 in ros2 (foxy)

---
+++ BASE_K8S-ROS2/build.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+################################################################################
+# ZHAW INIT
+# Description:  Shell script to create the Base GPU Docker image
+# Authors:      Leonardo Militano, Mark Straub, Giovanni Toffetti
+# Date:         2021-11-08
+################################################################################
+export ROS_DISTRO=foxy
+export IMAGE_NAME=robopaas/rosdocked-${ROS_DISTRO}-base-k8s:latest
+
+# Get this script's path
+pushd `dirname $0` > /dev/null
+SCRIPTPATH=`pwd`
+popd > /dev/null
+
+# Build the docker image
+docker build \
+  --no-cache \
+  --build-arg user=user\
+  --build-arg uid=$UID\
+  --build-arg home=/home/user \
+  --build-arg workspace=/home/user \
+  --build-arg shell=$SHELL\
+  -t $IMAGE_NAME .
---
+++ BASE_K8S-ROS2/start_desktop.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -x
+
+# Start XVnc/X/Lubuntu
+chmod -f 777 /tmp/.X11-unix
+# From: https://superuser.com/questions/806637/xauth-not-creating-xauthority-file (squashes complaints about .Xauthority)
+touch ~/.Xauthority
+xauth generate :0 . trusted
+/opt/TurboVNC/bin/vncserver -SecurityTypes vnc 
+
+# Start NoVNC. self.pem is a self-signed cert.
+if [ $? -eq 0 ] ; then
+    sudo /opt/noVNC/utils/launch.sh --vnc localhost:5901 --cert /home/ros/self.pem --listen 443;
+fi
