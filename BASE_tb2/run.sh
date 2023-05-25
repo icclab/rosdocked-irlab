eb42f177449283018a47cfc7ebeb10360fc9981b
Giovanni Toffetti <toff@zhaw.ch>
2023-05-25 07:56:04 +0200

    Added camera and compressed transport

--- BASE_tb2/run.sh
+++ BASE_tb2/run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-nvidia-docker run -it --privileged --net host robopaas/rosdocked-humble-tb2:latest
+nvidia-docker run -it --privileged --net host -v "$HOME:$HOME:rw" robopaas/rosdocked-humble-tb2:latest
