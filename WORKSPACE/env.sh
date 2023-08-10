f252737624c3c95553d3227b79a9207e74031a3d
Giovanni Toffetti <toff@zhaw.ch>
2023-08-10 11:56:57 +0200

    Added summit and TB2 to workspace

--- WORKSPACE/env.sh
+++ WORKSPACE/env.sh
@@ -1,7 +1,6 @@
 export ROS_DISTRO=humble
 export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
-export CYCLONEDDS_URI=file:///$(pwd)/cyclonedds.xml 
+export CYCLONEDDS_URI=file:///${HOME}/cyclonedds.xml 
 
-source /opt/ros/$ROS_DISTRO/setup.bash
 echo "** ROS2 $ROS_DISTRO initialized with $RMW_IMPLEMENTATION**"
 #source install/setup.bash
