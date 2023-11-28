95a354200787c61bb5fd83810d516ab2c5563199
Giovanni Toffetti <toff@zhaw.ch>
2023-11-28 11:33:59 +0100

    corrected wg0

--- BASE_tb2/start_tb2_VPN.sh
+++ BASE_tb2/start_tb2_VPN.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export CYCLONEDDS_URI=file:///home/turtlebot/cyclonedds.xml
+export CYCLONEDDS_URI=file:///home/ros/cyclonedds.xml
 launchfile="icclab_tb2_bringup_complete.launch.py"
 ros2 launch turtlebot2_bringup $launchfile &
 ROS_PID=$!
