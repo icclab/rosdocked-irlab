0f81bf57379992e2f4b6e2ca55ea840771561bc0
Giovanni Toffetti <toff@zhaw.ch>
2023-11-28 11:59:44 +0100

    OpenHorizon image name

--- BASE_tb2/build.sh
+++ BASE_tb2/build.sh
@@ -7,7 +7,7 @@
 # Date:         2021-11-08
 ################################################################################
 
-export IMAGE_NAME=robopaas/rosdocked-humble-tb2:latest
+export IMAGE_NAME=robopaas/rosdocked-humble-tb2_OH:latest
 
 # Get this script's path
 pushd `dirname $0` > /dev/null
