b9a84e8672fac27be628e278b3281eb26efe5171
miltzhaw <milt@zhaw.ch>
2024-08-20 12:35:50 +0200

    zenoh bridge conf and deb files

--- BASE_tb2/start_zenoh_tb2nomap_vo.sh
+++ BASE_tb2/start_zenoh_tb2nomap_vo.sh
@@ -11,7 +11,8 @@ VOWOT_PID=$!
 #launchfile="tb2_complete_no_map.launch.py"
 #ros2 launch turtlebot2_bringup $launchfile &
 #ROS_PID=$!
-/home/ros/zenoh-bridge-ros2dds -c zenoh-bridge-conf.json5 &
+#/home/ros/zenoh-bridge-ros2dds -c zenoh-bridge-conf.json5 &
+zenoh-bridge-ros2dds -c zenoh-bridge-conf.json5 &
 ZENOH_PID=$!
 #wait $ROS_PID
 wait $ZENOH_PID
