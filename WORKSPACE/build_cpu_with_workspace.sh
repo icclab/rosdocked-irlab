9cac881b2262a0dd4c083feef07420f8a5ad6728
janu <janu@zhaw.ch>
2025-02-06 15:19:22 +0100

    initial commit of mahroboter-jazzy

--- WORKSPACE/build_cpu_with_workspace.sh
+++ WORKSPACE/build_cpu_with_workspace.sh
@@ -2,13 +2,22 @@
 
 ################################################################################
 # ZHAW INIT
-# Description:  Shell script to create and add the required elements to the
-#               CPU image
-# Authors:      Leonardo Militano, Mark Straub, Giovanni Toffetti
+# Description:  Shell script to create and add the Mahroboter project elements
+#               to the CPU image
+# Authors:      Leonardo Militano, Mark Straub, Giovanni Toffetti, Stanislaw Januszko
 # Date:         2021-11-08
 ################################################################################
 
-export IMAGE_NAME=robopaas/rosdocked-jazzy-cpu:latest
+# Check if GITHUB_TOKEN (github.zhaw.ch) is provided
+if [ $# -eq 0 ]; then
+    echo "Error: Please provide the GitHub token as an argument"
+    echo "Usage: $0 <GITHUB_TOKEN>"
+    exit 1
+fi
+
+# Store the first argument as the GitHub token
+export GITHUB_TOKEN=$1
+export IMAGE_NAME=robopaas/rosdocked-jazzy-cpu-mahroboter:latest
 
 # Build the docker image
 docker build \
@@ -16,5 +25,6 @@ docker build \
   --build-arg USER=ros \
   --build-arg home=/home/ros \
   --build-arg workspace=/home/ros \
-  --build-arg shell=$SHELL\
+  --build-arg shell=$SHELL \
+  --build-arg GITHUB_TOKEN=$GITHUB_TOKEN \
   -t $IMAGE_NAME .
--- WORKSPACE/build_k8s_with_workspace.sh
+++ WORKSPACE/build_k8s_with_workspace.sh
@@ -2,17 +2,28 @@
 
 ################################################################################
 # ZHAW INIT
-# Description:  Shell script to create and add the required elements to the
-#               CPU image
-# Authors:      Leonardo Militano, Mark Straub, Giovanni Toffetti
+# Description:  Shell script to create and add the Mahroboter project elements
+#               to the K8S image
+# Authors:      Leonardo Militano, Mark Straub, Giovanni Toffetti, Stanislaw Januszko
 # Date:         2021-11-08
 ################################################################################
+
+# Check if GITHUB_TOKEN (github.zhaw.ch) is provided
+if [ $# -eq 0 ]; then
+    echo "Error: Please provide the GitHub token as an argument"
+    echo "Usage: $0 <GITHUB_TOKEN>"
+    exit 1
+fi
+
+# Store the first argument as the GitHub token
+export GITHUB_TOKEN=$1
 export CUDA_RELEASE=12.5.0
-export IMAGE_NAME=robopaas/rosdocked-jazzy-k8s:cuda${CUDA_RELEASE}
+export IMAGE_NAME=robopaas/rosdocked-jazzy-k8s-mahroboter:cuda${CUDA_RELEASE}
 
 # Build the docker image
 docker build \
   --build-arg BASE_IMAGE=robopaas/rosdocked-jazzy-base-k8s:cuda${CUDA_RELEASE} \
   --build-arg USER=ros \
   --build-arg shell=$SHELL\
+  --build-arg GITHUB_TOKEN=$GITHUB_TOKEN \
   -t $IMAGE_NAME .
