f53c9e85c6e7e7db152b86916a0d37ea731f4642
Leonardo Militano <milt@zhaw.ch>
2025-05-02 13:22:15 +0200

    two build for robopaas and nephele k8s cluster config for zenoh

--- BASE_summit/build_robopaas.sh
+++ BASE_summit/build_robopaas.sh
@@ -7,7 +7,7 @@
 # Date:         2021-11-08
 ################################################################################
 
-export IMAGE_NAME=robopaas/rosdocked-humble-nephele-summit:demo 
+export IMAGE_NAME=robopaas/rosdocked-humble-nephele-summit:robopaas 
 
 # Get this script's path
 pushd `dirname $0` > /dev/null
